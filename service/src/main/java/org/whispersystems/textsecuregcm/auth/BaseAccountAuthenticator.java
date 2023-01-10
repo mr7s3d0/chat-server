8549dd791a174ad507006ef0ffb2cadb34ab5791
mr7s3d0 <alice20bob@gmail.com>
2023-01-10 23:22:51 +0700

    Commit message

---
+++ service/src/main/java/org/whispersystems/textsecuregcm/auth/BaseAccountAuthenticator.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright 2013-2021 Signal Messenger, LLC
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+package org.whispersystems.textsecuregcm.auth;
+
+import static com.codahale.metrics.MetricRegistry.name;
+
+import com.google.common.annotations.VisibleForTesting;
+import io.dropwizard.auth.basic.BasicCredentials;
+import io.micrometer.core.instrument.Metrics;
+import io.micrometer.core.instrument.Tags;
+import java.time.Clock;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.Optional;
+import java.util.UUID;
+import org.apache.commons.lang3.StringUtils;
+import org.whispersystems.textsecuregcm.storage.Account;
+import org.whispersystems.textsecuregcm.storage.AccountsManager;
+import org.whispersystems.textsecuregcm.storage.Device;
+import org.whispersystems.textsecuregcm.storage.RefreshingAccountAndDeviceSupplier;
+import org.whispersystems.textsecuregcm.util.Pair;
+import org.whispersystems.textsecuregcm.util.Util;
+
+public class BaseAccountAuthenticator {
+
+  private static final String AUTHENTICATION_COUNTER_NAME = name(BaseAccountAuthenticator.class, "authentication");
+  private static final String AUTHENTICATION_SUCCEEDED_TAG_NAME = "succeeded";
+  private static final String AUTHENTICATION_FAILURE_REASON_TAG_NAME = "reason";
+  private static final String AUTHENTICATION_ENABLED_REQUIRED_TAG_NAME = "enabledRequired";
+  private static final String AUTHENTICATION_HAS_STORY_CAPABILITY = "hasStoryCapability";
+
+  private static final String STORY_ADOPTION_COUNTER_NAME = name(BaseAccountAuthenticator.class, "storyAdoption");
+
+  private static final String DAYS_SINCE_LAST_SEEN_DISTRIBUTION_NAME = name(BaseAccountAuthenticator.class, "daysSinceLastSeen");
+  private static final String IS_PRIMARY_DEVICE_TAG = "isPrimary";
+
+  private final AccountsManager accountsManager;
+  private final Clock           clock;
+
+  public BaseAccountAuthenticator(AccountsManager accountsManager) {
+    this(accountsManager, Clock.systemUTC());
+  }
+
+  @VisibleForTesting
+  public BaseAccountAuthenticator(AccountsManager accountsManager, Clock clock) {
+    this.accountsManager   = accountsManager;
+    this.clock             = clock;
+  }
+
+  static Pair<String, Long> getIdentifierAndDeviceId(final String basicUsername) {
+    final String identifier;
+    final long deviceId;
+
+    final int deviceIdSeparatorIndex = basicUsername.indexOf('.');
+
+    if (deviceIdSeparatorIndex == -1) {
+      identifier = basicUsername;
+      deviceId = Device.MASTER_ID;
+    } else {
+      identifier = basicUsername.substring(0, deviceIdSeparatorIndex);
+      deviceId = Long.parseLong(basicUsername.substring(deviceIdSeparatorIndex + 1));
+    }
+
+    return new Pair<>(identifier, deviceId);
+  }
+
+  public Optional<AuthenticatedAccount> authenticate(BasicCredentials basicCredentials, boolean enabledRequired) {
+    boolean succeeded = false;
+    String failureReason = null;
+    boolean hasStoryCapability = false;
+
+    try {
+      final UUID accountUuid;
+      final long deviceId;
+      {
+        final Pair<String, Long> identifierAndDeviceId = getIdentifierAndDeviceId(basicCredentials.getUsername());
+
+        accountUuid = UUID.fromString(identifierAndDeviceId.first());
+        deviceId = identifierAndDeviceId.second();
+      }
+
+      Optional<Account> account = accountsManager.getByAccountIdentifier(accountUuid);
+
+      if (account.isEmpty()) {
+        failureReason = "noSuchAccount";
+        return Optional.empty();
+      }
+
+      hasStoryCapability = account.map(Account::isStoriesSupported).orElse(false);
+
+      Optional<Device> device = account.get().getDevice(deviceId);
+
+      if (device.isEmpty()) {
+        failureReason = "noSuchDevice";
+        return Optional.empty();
+      }
+
+      if (enabledRequired) {
+        if (!device.get().isEnabled()) {
+          failureReason = "deviceDisabled";
+          return Optional.empty();
+        }
+
+        if (!account.get().isEnabled()) {
+          failureReason = "accountDisabled";
+          return Optional.empty();
+        }
+      }
+
+      AuthenticationCredentials deviceAuthenticationCredentials = device.get().getAuthenticationCredentials();
+      if (deviceAuthenticationCredentials.verify(basicCredentials.getPassword())) {
+        succeeded = true;
+        Account authenticatedAccount = updateLastSeen(account.get(), device.get());
+        if (deviceAuthenticationCredentials.getVersion() != AuthenticationCredentials.CURRENT_VERSION) {
+          authenticatedAccount = accountsManager.updateDeviceAuthentication(
+              authenticatedAccount,
+              device.get(),
+              new AuthenticationCredentials(basicCredentials.getPassword()));  // new credentials have current version
+        }
+        return Optional.of(new AuthenticatedAccount(
+            new RefreshingAccountAndDeviceSupplier(authenticatedAccount, device.get().getId(), accountsManager)));
+      }
+
+      return Optional.empty();
+    } catch (IllegalArgumentException | InvalidAuthorizationHeaderException iae) {
+      failureReason = "invalidHeader";
+      return Optional.empty();
+    } finally {
+      Tags tags = Tags.of(
+          AUTHENTICATION_SUCCEEDED_TAG_NAME, String.valueOf(succeeded),
+          AUTHENTICATION_ENABLED_REQUIRED_TAG_NAME, String.valueOf(enabledRequired));
+
+      if (StringUtils.isNotBlank(failureReason)) {
+        tags = tags.and(AUTHENTICATION_FAILURE_REASON_TAG_NAME, failureReason);
+      }
+
+      Metrics.counter(AUTHENTICATION_COUNTER_NAME, tags).increment();
+
+      Tags storyTags = Tags.of(AUTHENTICATION_HAS_STORY_CAPABILITY, String.valueOf(hasStoryCapability));
+      Metrics.counter(STORY_ADOPTION_COUNTER_NAME, storyTags).increment();
+    }
+  }
+
+  @VisibleForTesting
+  public Account updateLastSeen(Account account, Device device) {
+    // compute a non-negative integer between 0 and 86400.
+    long n = Util.ensureNonNegativeLong(account.getUuid().getLeastSignificantBits());
+    final long lastSeenOffsetSeconds = n % ChronoUnit.DAYS.getDuration().toSeconds();
+
+    // produce a truncated timestamp which is either today at UTC midnight
+    // or yesterday at UTC midnight, based on per-user randomized offset used.
+    final long todayInMillisWithOffset = Util.todayInMillisGivenOffsetFromNow(clock, Duration.ofSeconds(lastSeenOffsetSeconds).negated());
+
+    // only update the device's last seen time when it falls behind the truncated timestamp.
+    // this ensure a few things:
+    //   (1) each account will only update last-seen at most once per day
+    //   (2) these updates will occur throughout the day rather than all occurring at UTC midnight.
+    if (device.getLastSeen() < todayInMillisWithOffset) {
+      Metrics.summary(DAYS_SINCE_LAST_SEEN_DISTRIBUTION_NAME, IS_PRIMARY_DEVICE_TAG, String.valueOf(device.isMaster()))
+          .record(Duration.ofMillis(todayInMillisWithOffset - device.getLastSeen()).toDays());
+
+      return accountsManager.updateDeviceLastSeen(account, device, Util.todayInMillis(clock));
+    }
+
+    return account;
+  }
+}
---
+++ service/src/main/java/org/whispersystems/textsecuregcm/storage/SubscriptionManager.java
@@ -0,0 +1,457 @@
+/*
+ * Copyright 2021 Signal Messenger, LLC
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+package org.whispersystems.textsecuregcm.storage;
+
+import static org.whispersystems.textsecuregcm.util.AttributeValues.b;
+import static org.whispersystems.textsecuregcm.util.AttributeValues.m;
+import static org.whispersystems.textsecuregcm.util.AttributeValues.n;
+import static org.whispersystems.textsecuregcm.util.AttributeValues.s;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Throwables;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.time.Instant;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.whispersystems.textsecuregcm.subscriptions.ProcessorCustomer;
+import org.whispersystems.textsecuregcm.subscriptions.SubscriptionProcessor;
+import org.whispersystems.textsecuregcm.util.Pair;
+import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.ConditionalCheckFailedException;
+import software.amazon.awssdk.services.dynamodb.model.GetItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.GetItemResponse;
+import software.amazon.awssdk.services.dynamodb.model.QueryRequest;
+import software.amazon.awssdk.services.dynamodb.model.ReturnValue;
+import software.amazon.awssdk.services.dynamodb.model.UpdateItemRequest;
+
+public class SubscriptionManager {
+
+  private static final Logger logger = LoggerFactory.getLogger(SubscriptionManager.class);
+
+  private static final int USER_LENGTH = 16;
+
+  public static final String KEY_USER = "U";  // B  (Hash Key)
+  public static final String KEY_PASSWORD = "P";  // B
+  @Deprecated
+  public static final String KEY_CUSTOMER_ID = "C";  // S  (GSI Hash Key of `c_to_u` index)
+  public static final String KEY_PROCESSOR_ID_CUSTOMER_ID = "PC"; // B (GSI Hash Key of `pc_to_u` index)
+  public static final String KEY_CREATED_AT = "R";  // N
+  public static final String KEY_PROCESSOR_CUSTOMER_IDS_MAP = "PCI"; // M
+  public static final String KEY_SUBSCRIPTION_ID = "S";  // S
+  public static final String KEY_SUBSCRIPTION_CREATED_AT = "T";  // N
+  public static final String KEY_SUBSCRIPTION_LEVEL = "L";
+  public static final String KEY_SUBSCRIPTION_LEVEL_CHANGED_AT = "V";  // N
+  public static final String KEY_ACCESSED_AT = "A";  // N
+  public static final String KEY_CANCELED_AT = "B";  // N
+  public static final String KEY_CURRENT_PERIOD_ENDS_AT = "D";  // N
+
+  public static final String INDEX_NAME = "c_to_u";  // Hash Key "C"
+
+  public static class Record {
+
+    public final byte[] user;
+    public final byte[] password;
+    public final Instant createdAt;
+    @VisibleForTesting
+    @Nullable
+    ProcessorCustomer processorCustomer;
+    public Map<SubscriptionProcessor, String> processorsToCustomerIds;
+    public String subscriptionId;
+    public Instant subscriptionCreatedAt;
+    public Long subscriptionLevel;
+    public Instant subscriptionLevelChangedAt;
+    public Instant accessedAt;
+    public Instant canceledAt;
+    public Instant currentPeriodEndsAt;
+
+    private Record(byte[] user, byte[] password, Instant createdAt) {
+      this.user = checkUserLength(user);
+      this.password = Objects.requireNonNull(password);
+      this.createdAt = Objects.requireNonNull(createdAt);
+    }
+
+    public static Record from(byte[] user, Map<String, AttributeValue> item) {
+      Record record = new Record(
+          user,
+          item.get(KEY_PASSWORD).b().asByteArray(),
+          getInstant(item, KEY_CREATED_AT));
+
+      final Pair<SubscriptionProcessor, String> processorCustomerId = getProcessorAndCustomer(item);
+      if (processorCustomerId != null) {
+        record.processorCustomer = new ProcessorCustomer(processorCustomerId.second(), processorCustomerId.first());
+      }
+      record.processorsToCustomerIds = getProcessorsToCustomerIds(item);
+      record.subscriptionId = getString(item, KEY_SUBSCRIPTION_ID);
+      record.subscriptionCreatedAt = getInstant(item, KEY_SUBSCRIPTION_CREATED_AT);
+      record.subscriptionLevel = getLong(item, KEY_SUBSCRIPTION_LEVEL);
+      record.subscriptionLevelChangedAt = getInstant(item, KEY_SUBSCRIPTION_LEVEL_CHANGED_AT);
+      record.accessedAt = getInstant(item, KEY_ACCESSED_AT);
+      record.canceledAt = getInstant(item, KEY_CANCELED_AT);
+      record.currentPeriodEndsAt = getInstant(item, KEY_CURRENT_PERIOD_ENDS_AT);
+      return record;
+    }
+
+    public Optional<ProcessorCustomer> getProcessorCustomer() {
+      return Optional.ofNullable(processorCustomer);
+    }
+
+    private static Map<SubscriptionProcessor, String> getProcessorsToCustomerIds(Map<String, AttributeValue> item) {
+      final AttributeValue attributeValue = item.get(KEY_PROCESSOR_CUSTOMER_IDS_MAP);
+      final Map<String, AttributeValue> attribute =
+          attributeValue == null ? Collections.emptyMap() : attributeValue.m();
+
+      final Map<SubscriptionProcessor, String> processorsToCustomerIds = new HashMap<>();
+      attribute.forEach((processorName, customerId) ->
+          processorsToCustomerIds.put(SubscriptionProcessor.valueOf(processorName), customerId.s()));
+
+      return processorsToCustomerIds;
+    }
+
+    /**
+     * Extracts the active processor and customer from a single attribute value in the given item.
+     * <p>
+     * Until existing data is migrated, this may return {@code null}.
+     */
+    @Nullable
+    private static Pair<SubscriptionProcessor, String> getProcessorAndCustomer(Map<String, AttributeValue> item) {
+
+      final AttributeValue attributeValue = item.get(KEY_PROCESSOR_ID_CUSTOMER_ID);
+
+      if (attributeValue == null) {
+        // temporarily allow null values
+        return null;
+      }
+
+      final byte[] processorAndCustomerId = attributeValue.b().asByteArray();
+      final byte processorId = processorAndCustomerId[0];
+
+      final SubscriptionProcessor processor = SubscriptionProcessor.forId(processorId);
+      if (processor == null) {
+        throw new IllegalStateException("unknown processor id: " + processorId);
+      }
+
+      final String customerId = new String(processorAndCustomerId, 1, processorAndCustomerId.length - 1,
+          StandardCharsets.UTF_8);
+
+      return new Pair<>(processor, customerId);
+    }
+
+    private static String getString(Map<String, AttributeValue> item, String key) {
+      AttributeValue attributeValue = item.get(key);
+      if (attributeValue == null) {
+        return null;
+      }
+      return attributeValue.s();
+    }
+
+    private static Long getLong(Map<String, AttributeValue> item, String key) {
+      AttributeValue attributeValue = item.get(key);
+      if (attributeValue == null || attributeValue.n() == null) {
+        return null;
+      }
+      return Long.valueOf(attributeValue.n());
+    }
+
+    private static Instant getInstant(Map<String, AttributeValue> item, String key) {
+      AttributeValue attributeValue = item.get(key);
+      if (attributeValue == null || attributeValue.n() == null) {
+        return null;
+      }
+      return Instant.ofEpochSecond(Long.parseLong(attributeValue.n()));
+    }
+  }
+
+  private final String table;
+  private final DynamoDbAsyncClient client;
+
+  public SubscriptionManager(
+      @Nonnull String table,
+      @Nonnull DynamoDbAsyncClient client) {
+    this.table = Objects.requireNonNull(table);
+    this.client = Objects.requireNonNull(client);
+  }
+
+  /**
+   * Looks in the GSI for a record with the given customer id and returns the user id.
+   */
+  public CompletableFuture<byte[]> getSubscriberUserByStripeCustomerId(@Nonnull String customerId) {
+    QueryRequest query = QueryRequest.builder()
+        .tableName(table)
+        .indexName(INDEX_NAME)
+        .keyConditionExpression("#customer_id = :customer_id")
+        .projectionExpression("#user")
+        .expressionAttributeNames(Map.of(
+            "#customer_id", KEY_CUSTOMER_ID,
+            "#user", KEY_USER))
+        .expressionAttributeValues(Map.of(
+            ":customer_id", s(Objects.requireNonNull(customerId))))
+        .build();
+    return client.query(query).thenApply(queryResponse -> {
+      int count = queryResponse.count();
+      if (count == 0) {
+        return null;
+      } else if (count > 1) {
+        logger.error("expected invariant of 1-1 subscriber-customer violated for customer {}", customerId);
+        throw new IllegalStateException(
+            "expected invariant of 1-1 subscriber-customer violated for customer " + customerId);
+      } else {
+        Map<String, AttributeValue> result = queryResponse.items().get(0);
+        return result.get(KEY_USER).b().asByteArray();
+      }
+    });
+  }
+
+  public static class GetResult {
+
+    public static final GetResult NOT_STORED = new GetResult(Type.NOT_STORED, null);
+    public static final GetResult PASSWORD_MISMATCH = new GetResult(Type.PASSWORD_MISMATCH, null);
+
+    public enum Type {
+      NOT_STORED,
+      PASSWORD_MISMATCH,
+      FOUND
+    }
+
+    public final Type type;
+    public final Record record;
+
+    private GetResult(Type type, Record record) {
+      this.type = type;
+      this.record = record;
+    }
+
+    public static GetResult found(Record record) {
+      return new GetResult(Type.FOUND, record);
+    }
+  }
+
+  /**
+   * Looks up a record with the given {@code user} and validates the {@code hmac} before returning it.
+   */
+  public CompletableFuture<GetResult> get(byte[] user, byte[] hmac) {
+    return getUser(user).thenApply(getItemResponse -> {
+      if (!getItemResponse.hasItem()) {
+        return GetResult.NOT_STORED;
+      }
+
+      Record record = Record.from(user, getItemResponse.item());
+      if (!MessageDigest.isEqual(hmac, record.password)) {
+        return GetResult.PASSWORD_MISMATCH;
+      }
+      return GetResult.found(record);
+    });
+  }
+
+  private CompletableFuture<GetItemResponse> getUser(byte[] user) {
+    checkUserLength(user);
+
+    GetItemRequest request = GetItemRequest.builder()
+        .consistentRead(Boolean.TRUE)
+        .tableName(table)
+        .key(Map.of(KEY_USER, b(user)))
+        .build();
+
+    return client.getItem(request);
+  }
+
+  public CompletableFuture<Record> create(byte[] user, byte[] password, Instant createdAt) {
+    checkUserLength(user);
+
+    UpdateItemRequest request = UpdateItemRequest.builder()
+        .tableName(table)
+        .key(Map.of(KEY_USER, b(user)))
+        .returnValues(ReturnValue.ALL_NEW)
+        .conditionExpression("attribute_not_exists(#user) OR #password = :password")
+        .updateExpression("SET "
+            + "#password = if_not_exists(#password, :password), "
+            + "#created_at = if_not_exists(#created_at, :created_at), "
+            + "#accessed_at = if_not_exists(#accessed_at, :accessed_at), "
+            + "#processors_to_customer_ids = if_not_exists(#processors_to_customer_ids, :initial_empty_map)"
+        )
+        .expressionAttributeNames(Map.of(
+            "#user", KEY_USER,
+            "#password", KEY_PASSWORD,
+            "#created_at", KEY_CREATED_AT,
+            "#accessed_at", KEY_ACCESSED_AT,
+            "#processors_to_customer_ids", KEY_PROCESSOR_CUSTOMER_IDS_MAP)
+        )
+        .expressionAttributeValues(Map.of(
+            ":password", b(password),
+            ":created_at", n(createdAt.getEpochSecond()),
+            ":accessed_at", n(createdAt.getEpochSecond()),
+            ":initial_empty_map", m(Map.of()))
+        )
+        .build();
+    return client.updateItem(request).handle((updateItemResponse, throwable) -> {
+      if (throwable != null) {
+        if (Throwables.getRootCause(throwable) instanceof ConditionalCheckFailedException) {
+          return null;
+        }
+        Throwables.throwIfUnchecked(throwable);
+        throw new CompletionException(throwable);
+      }
+
+      return Record.from(user, updateItemResponse.attributes());
+    });
+  }
+
+  /**
+   * Updates the active processor and customer ID for the given user record.
+   *
+   * @return the updated user record.
+   */
+  public CompletableFuture<Record> updateProcessorAndCustomerId(Record userRecord,
+      ProcessorCustomer activeProcessorCustomer, Instant updatedAt) {
+
+    UpdateItemRequest request = UpdateItemRequest.builder()
+        .tableName(table)
+        .key(Map.of(KEY_USER, b(userRecord.user)))
+        .returnValues(ReturnValue.ALL_NEW)
+        .conditionExpression(
+            // there is no active processor+customer attribute
+            "attribute_not_exists(#processor_customer_id) " +
+                // or an attribute in the map with an inactive processor+customer
+                "AND attribute_not_exists(#processors_to_customer_ids.#processor_name)"
+        )
+        .updateExpression("SET "
+            + "#customer_id = :customer_id, "
+            + "#processor_customer_id = :processor_customer_id, "
+            + "#processors_to_customer_ids.#processor_name = :customer_id, "
+            + "#accessed_at = :accessed_at"
+        )
+        .expressionAttributeNames(Map.of(
+            "#accessed_at", KEY_ACCESSED_AT,
+            "#customer_id", KEY_CUSTOMER_ID,
+            "#processor_customer_id", KEY_PROCESSOR_ID_CUSTOMER_ID,
+            "#processor_name", activeProcessorCustomer.processor().name(),
+            "#processors_to_customer_ids", KEY_PROCESSOR_CUSTOMER_IDS_MAP
+        ))
+        .expressionAttributeValues(Map.of(
+            ":accessed_at", n(updatedAt.getEpochSecond()),
+            ":customer_id", s(activeProcessorCustomer.customerId()),
+            ":processor_customer_id", b(activeProcessorCustomer.toDynamoBytes())
+        )).build();
+
+    return client.updateItem(request)
+        .thenApply(updateItemResponse -> Record.from(userRecord.user, updateItemResponse.attributes()))
+        .exceptionallyCompose(throwable -> {
+          if (Throwables.getRootCause(throwable) instanceof ConditionalCheckFailedException) {
+            return getUser(userRecord.user).thenApply(getItemResponse ->
+                Record.from(userRecord.user, getItemResponse.item()));
+          }
+          Throwables.throwIfUnchecked(throwable);
+          throw new CompletionException(throwable);
+        });
+  }
+
+  public CompletableFuture<Void> accessedAt(byte[] user, Instant accessedAt) {
+    checkUserLength(user);
+
+    UpdateItemRequest request = UpdateItemRequest.builder()
+        .tableName(table)
+        .key(Map.of(KEY_USER, b(user)))
+        .returnValues(ReturnValue.NONE)
+        .updateExpression("SET #accessed_at = :accessed_at")
+        .expressionAttributeNames(Map.of("#accessed_at", KEY_ACCESSED_AT))
+        .expressionAttributeValues(Map.of(":accessed_at", n(accessedAt.getEpochSecond())))
+        .build();
+    return client.updateItem(request).thenApply(updateItemResponse -> null);
+  }
+
+  public CompletableFuture<Void> canceledAt(byte[] user, Instant canceledAt) {
+    checkUserLength(user);
+
+    UpdateItemRequest request = UpdateItemRequest.builder()
+        .tableName(table)
+        .key(Map.of(KEY_USER, b(user)))
+        .returnValues(ReturnValue.NONE)
+        .updateExpression("SET "
+            + "#accessed_at = :accessed_at, "
+            + "#canceled_at = :canceled_at "
+            + "REMOVE #subscription_id")
+        .expressionAttributeNames(Map.of(
+            "#accessed_at", KEY_ACCESSED_AT,
+            "#canceled_at", KEY_CANCELED_AT,
+            "#subscription_id", KEY_SUBSCRIPTION_ID))
+        .expressionAttributeValues(Map.of(
+            ":accessed_at", n(canceledAt.getEpochSecond()),
+            ":canceled_at", n(canceledAt.getEpochSecond())))
+        .build();
+    return client.updateItem(request).thenApply(updateItemResponse -> null);
+  }
+
+  public CompletableFuture<Void> subscriptionCreated(
+      byte[] user, String subscriptionId, Instant subscriptionCreatedAt, long level) {
+    checkUserLength(user);
+
+    UpdateItemRequest request = UpdateItemRequest.builder()
+        .tableName(table)
+        .key(Map.of(KEY_USER, b(user)))
+        .returnValues(ReturnValue.NONE)
+        .updateExpression("SET "
+            + "#accessed_at = :accessed_at, "
+            + "#subscription_id = :subscription_id, "
+            + "#subscription_created_at = :subscription_created_at, "
+            + "#subscription_level = :subscription_level, "
+            + "#subscription_level_changed_at = :subscription_level_changed_at")
+        .expressionAttributeNames(Map.of(
+            "#accessed_at", KEY_ACCESSED_AT,
+            "#subscription_id", KEY_SUBSCRIPTION_ID,
+            "#subscription_created_at", KEY_SUBSCRIPTION_CREATED_AT,
+            "#subscription_level", KEY_SUBSCRIPTION_LEVEL,
+            "#subscription_level_changed_at", KEY_SUBSCRIPTION_LEVEL_CHANGED_AT))
+        .expressionAttributeValues(Map.of(
+            ":accessed_at", n(subscriptionCreatedAt.getEpochSecond()),
+            ":subscription_id", s(subscriptionId),
+            ":subscription_created_at", n(subscriptionCreatedAt.getEpochSecond()),
+            ":subscription_level", n(level),
+            ":subscription_level_changed_at", n(subscriptionCreatedAt.getEpochSecond())))
+        .build();
+    return client.updateItem(request).thenApply(updateItemResponse -> null);
+  }
+
+  public CompletableFuture<Void> subscriptionLevelChanged(
+      byte[] user, Instant subscriptionLevelChangedAt, long level) {
+    checkUserLength(user);
+
+    UpdateItemRequest request = UpdateItemRequest.builder()
+        .tableName(table)
+        .key(Map.of(KEY_USER, b(user)))
+        .returnValues(ReturnValue.NONE)
+        .updateExpression("SET "
+            + "#accessed_at = :accessed_at, "
+            + "#subscription_level = :subscription_level, "
+            + "#subscription_level_changed_at = :subscription_level_changed_at")
+        .expressionAttributeNames(Map.of(
+            "#accessed_at", KEY_ACCESSED_AT,
+            "#subscription_level", KEY_SUBSCRIPTION_LEVEL,
+            "#subscription_level_changed_at", KEY_SUBSCRIPTION_LEVEL_CHANGED_AT))
+        .expressionAttributeValues(Map.of(
+            ":accessed_at", n(subscriptionLevelChangedAt.getEpochSecond()),
+            ":subscription_level", n(level),
+            ":subscription_level_changed_at", n(subscriptionLevelChangedAt.getEpochSecond())))
+        .build();
+    return client.updateItem(request).thenApply(updateItemResponse -> null);
+  }
+
+  private static byte[] checkUserLength(final byte[] user) {
+    if (user.length != USER_LENGTH) {
+      throw new IllegalArgumentException("user length is wrong; expected " + USER_LENGTH + "; was " + user.length);
+    }
+    return user;
+  }
+}
